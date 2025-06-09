5ce22d14b32b52742ef5c2abe249bc04be01aaef
corpuzJai <corpuz.jianna@gmail.com>
2025-06-09 22:39:39 +0800

    created a simple GUI window

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+root.geometry("600x500")
+root.title("Empty Desk")
+# Creating Label Widget
+myLabel = Label(root, text="Hello World!")
+
+# Adding Lable in screen
+myLabel.pack()
+
+root.mainloop()
