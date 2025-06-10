48ab9be46ae6cc7bb4d738bc00765c33dcfd0a84
corpuzJai <corpuz.jianna@gmail.com>
2025-06-10 20:47:13 +0800

    created new branch to test calculator

--- main.py
+++ main.py
@@ -2,37 +2,10 @@ import tkinter as tk
 from tkcalendar import Calendar, DateEntry
 
 root = tk.Tk()
-root.geometry("600x500") # Set window size
+root.geometry("400x500") # Set window size
 root.title("Empty Desk") # Set window title
 
-# Functions
-def addClass():
-    newClass = tk.Label(root, text=entry.get())
-    newClass.pack()
 
 
-# Input Fields 
-entry = tk.Entry(root)
-# Creating Label Widget
-myLabel = tk.Label(root,
-                text="Hello World!",
-                font=('Arial', 18)
-                )
-
-myLabel2 = tk.Label(root,
-                text="This tracks class attendance"
-                )
-
-myButton = tk.Button(root,
-                text="Add Class",
-                padx=15,
-                command=addClass
-                )
-
-# Adding Lable in screen
-myLabel.pack()
-myLabel2.pack()
-myButton.pack()
-entry.pack()
 
 root.mainloop()
