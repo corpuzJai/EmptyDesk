c45b907d9091432221ab95c962962b4ce71a64c4
corpuzJai <corpuz.jianna@gmail.com>
2025-06-10 19:29:39 +0800

    added label, button, and input field

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from tkinter import *
+import tkinter as tk
+from tkcalendar import Calendar, DateEntry
 
-root = Tk()
+root = tk.Tk()
+root.geometry("600x500") # Set window size
+root.title("Empty Desk") # Set window title
 
-root.geometry("600x500")
-root.title("Empty Desk")
+# Functions
+def addClass():
+    newClass = tk.Label(root, text=entry.get())
+    newClass.pack()
+
+
+# Input Fields 
+entry = tk.Entry(root)
 # Creating Label Widget
-myLabel = Label(root, text="Hello World!")
+myLabel = tk.Label(root,
+                text="Hello World!",
+                font=('Arial', 18)
+                )
+
+myLabel2 = tk.Label(root,
+                text="This tracks class attendance"
+                )
+
+myButton = tk.Button(root,
+                text="Add Class",
+                padx=15,
+                command=addClass
+                )
 
 # Adding Lable in screen
 myLabel.pack()
+myLabel2.pack()
+myButton.pack()
+entry.pack()
 
 root.mainloop()
