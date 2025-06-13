83bafef975bfd8e0c8db5b1d282d8789a6a2a03c
corpuzJai <corpuz.jianna@gmail.com>
2025-06-13 13:34:12 +0800

    added add class funtion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 from tkcalendar import Calendar
 
 root = tk.Tk()
@@ -6,19 +7,24 @@ root.geometry("580x400") # Set window size
 root.title("Empty Desk") # Set window title
 
 # ----- Class Dropdown -----
-classes = [
-    "CSCI 199.2",
-    "CSCI 60",
-    "CSCI 70",
-    "CSCI 41",
-    "CSCI 50i",
-    "DLQ 10",
-    "ISCS 30.XX"
-]
+# classes = [
+#     "CSCI 199.2",
+#     "CSCI 60",
+#     "CSCI 70",
+#     "CSCI 41",
+#     "CSCI 50i",
+#     "DLQ 10",
+#     "ISCS 30.XX"
+# ]
+classes = []
 selected_class = tk.StringVar()
-selected_class.set(classes[0])
+if not classes:
+    selected_class.set("No Classes, add one now!")
+else:
+    selected_class.set(classes[0])
 
-class_dropdown = tk.OptionMenu(root, selected_class, *classes)
+menu_options = classes if classes else["No classes"]
+class_dropdown = tk.OptionMenu(root, selected_class, *menu_options)
 class_dropdown.grid(row=0, column=0)
 
 # ----- Calendar -----
@@ -33,6 +39,73 @@ class_status = tk.Label(root, text="", bg="grey", fg="white", width=30, height=1
 class_status.grid(row=1, column=0, rowspan=3)
 
 # ----- Functions -----
+def add_course_window():
+    def save_course():
+        course = course_entry.get()
+        prof = prof_entry.get()
+        email = email_entry.get()
+        room = room_entry.get()
+        limit = limit_entry.get()
+
+        if not course or not limit.isdigit():
+            tk.messagebox.showerror("Invalid Input", "Please class name and valid number")
+            return
+
+        absents[course] = {
+            "dates": [],
+            "limit": int(limit),
+            "prof": prof,
+            "email": email,
+            "room": room
+        }
+
+        messagebox.showinfo("Sucess!", f"Class '{course}' added.")
+        add_course_window.destroy()
+        update_class_dropdown()
+
+    add_course_window = tk.Toplevel(root)
+    add_course_window.title("Add New Class")
+    add_course_window.geometry("300x250")
+
+    course_name = tk.Label(add_course_window, text="Class Name: ")
+    course_entry = tk.Entry(add_course_window)
+    course_name.grid(row =0, column =0)
+    course_entry.grid(row=0, column=1)
+
+    prof_name = tk.Label(add_course_window, text="Professor: ")
+    prof_entry = tk.Entry(add_course_window)
+    prof_name.grid(row=1, column =0)
+    prof_entry.grid(row=1, column=1)
+
+    email_ad= tk.Label(add_course_window, text="Email: ")
+    email_entry = tk.Entry(add_course_window)
+    email_ad.grid(row=2, column =0)
+    email_entry.grid(row=2, column =1)
+
+    room_num = tk.Label(add_course_window, text="Room: ")
+    room_entry = tk.Entry(add_course_window)
+    room_num.grid(row=3, column =0)
+    room_entry.grid(row=3, column =1)
+
+    limit_num = tk.Label(add_course_window, text="Cut Limit: ")
+    limit_entry = tk.Entry(add_course_window)
+    limit_num.grid(row=4, column =0)
+    limit_entry.grid(row=4, column =1)
+
+    add_course_button = tk.Button(add_course_window, text="Add Course", command = save_course)
+    add_course_button.grid(row=5, column=1)
+
+def remove_course_window():
+    def remove_course():
+        return
+    return
+
+def update_class_dropdown():
+    menu = class_dropdown["menu"]
+    menu.delete(0, "end")
+    for class_name in absents:
+        menu.add_command(label=class_name, command=lambda value=class_name: selected_class.set(value))
+
 def add_absent():
     class_name = selected_class.get()
     date = cal.get_date()
@@ -83,6 +156,7 @@ def show_all_absents():
     report_text.insert(tk.END, f"{'TOTAL CUTS':<25}{total_absents:<15}\n")
     report_text.config(state="disabled")
 
+
 # ----- Buttons -----
 add_absent_button = tk.Button(root, text="Add Absent", command=add_absent, pady=10)
 add_absent_button.grid(row=1, column=1)
@@ -92,6 +166,9 @@ show_absents_button.grid(row=2, column=1)
 
 show_report_button = tk.Button(root, text = "All Empty Desks", command=show_all_absents, pady=10)
 show_report_button.grid(row=3, column=1)
+
+add_class_button = tk.Button(root, text = "Add New Course", command=add_course_window)
+add_class_button.grid(row=4, column=1)
 
 root.focus_force()
 root.lift()
